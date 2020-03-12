6add42f6b48feb1538a8acb475461e6ff9af64b6
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-03-12 17:50:54 -0500

    base structure, ran into issues with code given, need to follow up with TA

---
+++ models/index.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number
+        }
+    ]
+});
+
+var Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout
